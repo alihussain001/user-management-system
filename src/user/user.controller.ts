1a071120214f939a15a9aa803b96aa79100016eb
alihussain <ahalihussain11@gmail.com>
2025-12-31 22:58:14 +0500

    get user and add user service and controllers added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService){}
+
+    @Get()
+    getUser(){
+        return this.userService.getAllUser();
+    };
+
+    @Post()
+    addUser(@Body() createUserDTo:CreateUserDto){
+        return this.userService.addUser(createUserDTo);
+    }
+}
